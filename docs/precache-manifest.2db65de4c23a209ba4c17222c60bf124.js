4051ff13b006b9761d42465528cade31cb942dcc
alhana-yo <beatriz.german.rengel@gmail.com>
2019-01-14 09:40:47 +0100

    Docs added second time

---
+++ docs/precache-manifest.2db65de4c23a209ba4c17222c60bf124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f9d381e7f827f4d2a0f",
+    "url": "./static/css/main.098060ca.chunk.css"
+  },
+  {
+    "revision": "5f9d381e7f827f4d2a0f",
+    "url": "./static/js/main.5f9d381e.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f9538b1c7ade7445544af3e934e470e4",
+    "url": "./index.html"
+  }
+];
